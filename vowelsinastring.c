8f7b915f306b0cc644dcef61f1cf76dadf0d4059
hrithick157 <77242622+hrithick157@users.noreply.github.com>
2021-01-14 00:20:28 +0530

    Update vowelsinastring.c

--- vowelsinastring.c
+++ vowelsinastring.c
@@ -6,7 +6,7 @@ void main()
 	int t,i,vow=0;
 	printf("\nEnter the string: ");
 	gets(a);
-    t=strlen(a);
+        t=strlen(a);
 	for(i=0;i<t;i++)
 	{
                         if(a[i]=='a' || a[i]=='A' || a[i]=='e' || a[i]=='E' || a[i]=='i' || a[i]=='I' || a[i]=='o' || a[i]=='O' || a[i]=='u' || a[i]=='U')
